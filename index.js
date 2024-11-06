c52e56b1020d17579b4a1353ee820c12ddbbe152
Michael Pina <mpina1994@aol.com>
2024-11-05 21:14:11 -0600

    lots of changes made input drop down selects hosted on github pages as well nice

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const demonHunter = classDamageAndTankPickSpec = (needTank) => {
 
 const warrior = classDamageAndTankPickSpec = (needTank) => {
     if (needTank) {
-        return "Protection"
+        return "Protection Warrior"
     }
     else {
         const specChooser = getRandomNumber(1, 2);
@@ -119,14 +119,14 @@ const priest = classDamageAndHealerPickSpec = (needHeals) => {
             return "Discipline"
         }
         else {
-            return "Holy"
+            return "Holy Priest"
         }
     }
 }
 
 const shaman = classDamageAndHealerPickSpec = (needHeals) => {
     if (needHeals) {
-        return "Restoration"
+        return "Restoration Shaman"
     }
     else {
         const specChooser = getRandomNumber(1, 2);
@@ -183,81 +183,205 @@ const paladin = classDamageAndHealerAndTankPickSpec = (needHeals, needTank) => {
     }
 }
 
+const getClass = (isTank, isHealer, theClass) => {
+    if (isTank) {
+        switch (theClass) {
+            case "Death Knight":
+                return deathKnight(isTank);
+                break;
+            case "Demon Hunter":
+                return demonHunter(isTank);
+                break;
+            case "Druid":
+                return druid(isHealer, isTank);
+                break;
+            case "Monk":
+                return monk(isHealer, isTank);
+                break;
+            case "Paladin":
+                return paladin(isHealer, isTank);
+                break;
+            case "Warrior":
+                return warrior(isTank);
+                break;
+            default:
+                return "Typo in a class name";
+
+        }
+
+    }
+    if (isHealer) {
+        switch (theClass) {
+            case "Druid":
+                return druid(isHealer, isTank);
+                break;
+            case "Evoker":
+                return evoker(isHealer);
+                break;
+            case "Monk":
+                return monk(isHealer, isTank);
+                break;
+            case "Paladin":
+                return paladin(isHealer, isTank);
+                break;
+            case "Priest":
+                return priest(isHealer);
+                break;
+            case "Shaman":
+                return shaman(isHealer);
+                break;
+            default:
+                return "Typo in a class name";
+        }
+    }
+    switch (theClass) {
+        case "Death Knight":
+            return deathKnight(isTank);
+            break;
+        case "Demon Hunter":
+            return demonHunter(isTank);
+            break;
+        case "Druid":
+            return druid(isHealer, isTank);
+            break;
+        case "Evoker":
+            return evoker(isHealer);
+            break;
+        case "Hunter":
+            return hunter();
+            break;
+        case "Mage":
+            return mage();
+            break;
+        case "Monk":
+            return monk(isHealer, isTank);
+            break;
+        case "Paladin":
+            return paladin(isHealer, isTank);
+            break;
+        case "Priest":
+            return priest(isHealer);
+            break;
+        case "Rogue":
+            return rogue();
+            break;
+        case "Shaman":
+            return shaman(isHealer);
+            break;
+        case "Warlock":
+            return warlock();
+            break;
+        case "Warrior":
+            return warrior(isTank);
+            break;
+        default:
+            return "Typo in a class name";
+    }
+}
 
 const validComp = (theGroup) => {
-    if (theGroup.length != 5) {
-        return [false, false];
-    }
-
-    let numHealersPossible = 0;
-    let numTanksPossible = 0;
-
     let tankList = [];
     let healerList = [];
+    let overlapList = [];
     let tank;
     let healer;
+
     for (let index = 0; index < theGroup.length; index++) {
         const className = theGroup[index];
         if (className == "Paladin" || className == "Warrior" || className == "Monk" || className == "Druid" || className == "Death Knight" || className == "Demon Hunter") {
-            numTanksPossible++;
+            if (className == "Paladin" || className == "Monk" || className == "Druid") {
+                overlapList.push(className); //logs how many classes overlap on tank and healer in the grp
+            }
             tankList.push(className);
         }
-    }
+    }                                               //these two for loops will determine the possible list of tanks and healers potentially in the group list
     if (tankList == 0) {
-        return [false, false];
-    }
-    tank = tankList[getRandomNumber(0, tankList.length - 1)]
-    let personInGroup = theGroup.indexOf(tank);
-    if (personInGroup > -1) { // only splice array when item is found
-        theGroup.splice(personInGroup, 1); // 2nd parameter means remove one item only
+        return false;
     }
     for (let index = 0; index < theGroup.length; index++) {
         const className = theGroup[index];
         if (className == "Paladin" || className == "Evoker" || className == "Monk" || className == "Druid" || className == "Priest" || className == "Shaman") {
-            numHealersPossible++;
             healerList.push(className);
         }
     }
     if (healerList == 0) {
-        return [false, false];
+        return false;
+    }
+
+    if (overlapList.length == 1) { //if there is only 1 overlap between check which has more possible choices, the larger gets the overlapped removed from their pool
+        if (tankList.length > healerList.length) {
+            tankList.splice(overlapList[0], 1)
+        }
+        else {
+            healerList.splice(overlapList[0], 1)
+        }
+
+        tank = tankList[getRandomNumber(0, tankList.length - 1)]
+        let personInGroup = theGroup.indexOf(tank);
+        if (personInGroup > -1) { // only splice array when item is found
+            theGroup.splice(personInGroup, 1); // 2nd parameter means remove one item only
+        }
+    }
+    else {
+        tank = tankList[getRandomNumber(0, tankList.length - 1)]
+        let personInGroup = theGroup.indexOf(tank);
+        if (personInGroup > -1) { // only splice array when item is found
+            theGroup.splice(personInGroup, 1); // 2nd parameter means remove one item only
+        }
+        personInGroup = healerList.indexOf(tank);
+        if (personInGroup > -1) { // only splice array when item is found
+            healerList.splice(personInGroup, 1); // 2nd parameter means remove one item only
+        }
     }
     healer = healerList[getRandomNumber(0, healerList.length - 1)]
     personInGroup = theGroup.indexOf(healer);
     if (personInGroup > -1) { // only splice array when item is found
         theGroup.splice(personInGroup, 1); // 2nd parameter means remove one item only
     }
-    return [healer, tank];
+
+    const tankSpec = getClass(true, false, tank); //invoke for tank and healer
+    const healerSpec = getClass(false, true, healer);
+    let grpSpec = [];
+    grpSpec.push(tankSpec);
+    grpSpec.push(healerSpec);
+    for (let index = 0; index < theGroup.length; index++) { //invoke for rest of group
+        grpSpec.push(getClass(false, false, theGroup[index]));
+    }
+
+    return grpSpec;
 }
 
-const groupSpecPicker = (group) => {
-    const [tank, healer] = validComp(group);
-    if (tank == false) {
-        return "not a valid comp"
-    }
-    else {
-        return "yay"
-    }
-}
+
 
 
 const button1 = document.createElement("button");
 document.body.appendChild(button1);
 button1.textContent = "run the roulette";
 button1.onclick = onbuttonclicked;
+let output;
+
 
 function onbuttonclicked() {
-    const inputElement1 = document.getElementById("input1");
-    const inputValue1 = inputElement1.value;
-    const inputElement2 = document.getElementById("input2");
-    const inputValue2 = inputElement2.value;
-    const inputElement3 = document.getElementById("input3");
-    const inputValue3 = inputElement3.value;
-    const inputElement4 = document.getElementById("input4");
-    const inputValue4 = inputElement4.value;
-    const inputElement5 = document.getElementById("input5");
-    const inputValue5 = inputElement5.value;
+    const selectElement1 = document.getElementById("class1");
+    const selectValue1 = selectElement1.value;
+    const selectElement2 = document.getElementById("class2");
+    const selectValue2 = selectElement2.value;
+    const selectElement3 = document.getElementById("class3");
+    const selectValue3 = selectElement3.value;
+    const selectElement4 = document.getElementById("class4");
+    const selectValue4 = selectElement4.value;
+    const selectElement5 = document.getElementById("class5");
+    const selectValue5 = selectElement5.value;
 
 
-    const group = [inputValue1, inputValue2, inputValue3, inputValue4, inputValue5];
-    console.log(groupSpecPicker(group))
+    const group = [selectValue1, selectValue2, selectValue3, selectValue4, selectValue5];
+    const grpSpec = (validComp(group));
+    if (grpSpec[0] == undefined) {
+        output = "Not a valid comp"
+        document.getElementById("output").innerHTML = output;
+    }
+    else {
+        output = `Tank: ${grpSpec[0]} Healer: ${grpSpec[1]} The damage: ${grpSpec[2]}, ${grpSpec[3]}, ${grpSpec[4]}`
+        document.getElementById("output").innerHTML = output;
+    }
 }

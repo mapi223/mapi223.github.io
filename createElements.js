c51de8ae2005a600a7d5ce4c4723ff9b26093b4f
Michael Pina <mpina1994@aol.com>
2024-11-20 23:50:28 -0600

    Fized some ish, added some gap in buttonContainer, handled input submits and getting values from buttons.

--- createElements.js
+++ createElements.js
@@ -1,16 +1,25 @@
 const classArray = ["deathKnight", "demonHunter", "druid", "evoker", "hunter", "mage", "monk", "paladin", "priest", "rogue", "shaman", "warlock", "warrior"];
 const classArrayWithSpacesAndCaps = ["Death Knight", "Demon Hunter", "Druid", "Evoker", "Hunter", "Mage", "Monk", "Paladin", "Priest", "Rogue", "Shaman", "Warlock", "Warrior"];
 
+const Player1Array = [];
+const Player2Array = [];
+const Player3Array = [];
+const Player4Array = [];
+const Player5Array = [];
+const players = [Player1Array, Player2Array, Player3Array, Player4Array, Player5Array];
 
 const toggleClassSelection = (source) => {
     const button = source.srcElement;
-    if (button.id === 'false') {
+    const playerNum = button.parentElement.parentElement.id[6];
+    if (button.getAttribute('flag') === 'false') {
         button.classList.add('active')
-        button.id = 'true';
+        button.setAttribute("flag", 'true');
+        players[playerNum - 1].push(button.classList[1]);
     }
-    else if (button.id === 'true') {
+    else if (button.getAttribute('flag') === 'true') {
+        players[playerNum - 1].splice(players[playerNum - 1].indexOf(button.classList[1]), 1);
         button.classList.remove('active');
-        button.id = 'false';
+        button.setAttribute("flag", 'false');
     }
 }
 
@@ -26,9 +35,15 @@ const fillForm = (index) => {
     input.type = 'text';
     input.placeholder = 'Enter Player Name';
     input.id = "StopYellingAtMe";
+    input.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            changePlayerName(event);
+        }
+    });
     form.appendChild(input);
     const button = document.createElement('button');
-    button.innerHTML = "Change Player Name!";
+    button.innerHTML = "Enter";
     button.className = 'PlayerName';
     button.type = "button"
     button.onclick = changePlayerName;
@@ -37,6 +52,10 @@ const fillForm = (index) => {
     lineBreak.id = "lineBreak";
     lineBreak.innerHTML = '<br>';
     form.appendChild(lineBreak);
+    const div = document.createElement('div');
+    div.id = "buttonContainer";
+    div.className = "buttonContainer";
+    form.appendChild(div);
     return form;
 }
 
@@ -55,10 +74,10 @@ const changePlayerName = (source) => {
 const createButton = (index) => {
     const button = document.createElement('button');
     button.type = "button"
-    document.body.children[2].children[index].appendChild(button);
+    document.body.children[2].children[index].children[4].appendChild(button);
     button.classList.add("classButton");
     button.onclick = toggleClassSelection;
-    button.id = "false";
+    button.setAttribute("flag", "false");
 
     return button;
 }
